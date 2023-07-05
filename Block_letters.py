3a1bfb5b36f215490c76c296c136d99227ea7f61
Sengeki1 <gamersengeki@gmail.com>
2023-07-05 22:58:37 -0100

    finished

---
+++ Block_letters.py
@@ -0,0 +1,23 @@
+
+init = """
+M       M
+M M   M M
+M M   M M
+M   M   M
+M       M
+M       M
+M       M
+"""
+
+init2 = """
+ SSS
+ S   S 
+ S     
+  SSS 
+     S
+ S   S
+  SSS  
+"""
+
+print(init)
+print(init2)
